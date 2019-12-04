7eaa363961e2fcbe3ff3f799efcf7591cd2cdbbd
biohzrdmx <biohazardmx@gmail.com>
2019-12-04 14:29:15 -0600

    Renamed plugin files to match my other plugins, added Settings link in plugins page

--- recaptcha.php
+++ recaptcha.php
@@ -15,7 +15,7 @@
 
 			public static function actionHead() {
 				$dir = plugin_dir_url(__FILE__);
-				wp_enqueue_script( 'recaptcha-js', "{$dir}plugin.js", ['jquery'], '1.0', true );
+				wp_enqueue_script( 'recaptcha-js', "{$dir}recaptcha.js", ['jquery'], '1.0', true );
 				wp_enqueue_script( 'recaptcha', 'https://www.google.com/recaptcha/api.js?onload=onLoadRecaptcha', [], '1.0', true );
 			}
 
@@ -28,6 +28,16 @@
 				add_settings_section( 'recaptcha_settings', __( 'General settings', 'recaptcha' ), 'reCAPTCHA::callbackSettings', 'recaptcha' );
 				add_settings_field( 'recaptcha_field_site', __('Site key', 'recaptcha'), 'reCAPTCHA::fieldText', 'recaptcha', 'recaptcha_settings', [ 'label_for' => 'recaptcha_field_site', 'class' => 'recaptcha_row' ]);
 				add_settings_field( 'recaptcha_field_secret', __('Secret key', 'recaptcha'), 'reCAPTCHA::fieldText', 'recaptcha', 'recaptcha_settings', [ 'label_for' => 'recaptcha_field_secret', 'class' => 'recaptcha_row' ] );
+			}
+
+			public static function adminSettingsLink($links, $file) {
+				$links = (array) $links;
+				if ( $file === 'wp-recaptcha/recaptcha.php' && current_user_can( 'manage_options' ) ) {
+					$url = admin_url('admin.php?page=recaptcha');
+					$link = sprintf( '<a href="%s">%s</a>', $url, __( 'Settings', 'recaptcha' ) );
+					array_unshift($links, $link);
+				}
+				return $links;
 			}
 
 			public static function fieldText($args) {
@@ -116,6 +126,7 @@
 		add_action( 'wp_head', 'reCAPTCHA::actionHead' );
 		add_action( 'admin_init', 'reCAPTCHA::actionAdminInit' );
 		add_action( 'admin_menu', 'reCAPTCHA::actionAdminMenu' );
+		add_filter( 'plugin_action_links', 'reCAPTCHA::adminSettingsLink', 10, 5 );
 
 	}
 
